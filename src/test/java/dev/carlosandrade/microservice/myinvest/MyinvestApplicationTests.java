dd4401b16c07be5965f733c71b0ecf60fd8ebd34
Carlos Andrade <dev_carlos@outlook.com>
2022-11-10 17:41:46 -0300

    primeiro comit

---
+++ src/test/java/dev/carlosandrade/microservice/myinvest/MyinvestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carlosandrade.microservice.myinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
